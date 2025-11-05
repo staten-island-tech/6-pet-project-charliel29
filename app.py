ab87f3e8866748285240940756f0a0adf6c83d7b
charliel29 <charliel29@nycstudents.net>
2025-11-05 09:26:21 -0500

    pet project

--- app.py
+++ app.py
@@ -1,3 +1,29 @@
+""" class Hero:
+    def __init__(self, name, money, inventory):
+        self.name = name
+        self.money = money
+        self.inventory = inventory
+
+    def buy(self, item):
+        self.inventory.append(item)
+        print(self.inventory)
+Jillian = Hero("Jillian" , 150, ["Potion"])
+Jillian.buy({"title": "Sword", "atk": 34})
+print(Jillian.__dict__)
+ """
+
+""" class BankAccount:
+    def __init__(self, owner, balance):
+        self.owner = owner
+        self.__balance = balance  
+    def deposit(self, amount):
+        self.__balance += amount
+    def show_balance(self):
+        print(f"{self.owner} has ${self.__balance}")
+ """
+
+
+
 class Hero:
     def __init__(self, name, money, inventory):
         self.name = name
@@ -7,5 +33,5 @@ class Hero:
     def buy(self, item):
         self.inventory.append(item)
         print(self.inventory)
-Charlie = Hero("Charlie", 100 , ["Ball"])
-Charlie.buy({"title": "Sword", "atk" :34})
+
+ 
