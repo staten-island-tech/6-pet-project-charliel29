c093854f361212a949b15ac9d9545810b9c80d70
charliel29 <charliel29@nycstudents.net>
2025-12-08 09:23:13 -0500

    done

--- app.py
+++ app.py
@@ -53,15 +53,15 @@ class Pet:
             game = input("Choose what you want to do")
             if game == "1":
                 self.happiness += 3
-                valid == False
+                valid = False
                 print(f"{self.name} played fetch! Gained 3 Happiness")
             elif game == "2":
                 self.happiness += 3
-                valid == False
+                valid = False
                 print(f"{self.name} Played tug of war! Gained 3 Happiness")
             elif game == "3":
                 self.happiness += 3
-                valid == False
+                valid = False
                 print(f"{self.name} Played catch! Gained 3 Happines")
             else:
                 print("You must choose a option 1,2,or 3")
@@ -76,21 +76,21 @@ class Pet:
             feed = input("What would you like to feed your pet")
             if feed == "1":
                 self.hunger -=3
-                valid == False
+                valid = False
                 if self.hunger<0:
                     self.hunger=0
                 print(f"after eating meat{self.name} lost 3 hunger")
             elif feed == "2":
                 self.hunger -=2
+                valid = False
                 if self.hunger<0:
                     self.hunger=0
-                valid == False
                 print(f"after eating vegatble {self.name} lost 2 hunger")
             elif feed == "3":
                 self.hunger -=1
+                valid = False
                 if self.hunger<0:
                     self.hunger=0
-                valid == False
                 print(f"after drinking water {self.name} lost 1 hunger")
             else:
                 print("You must choose a option 1,2, or 3")
@@ -104,18 +104,18 @@ class Pet:
             clean = input("How would you like to clean your pet?")
             if clean == "1":
                 self.cleansiness +=3
-                valid == False
-            if self.cleansiness<0:
-                self.cleansiness=0
-            print(f"after washing {self.name} lost 3 cleansiness")
+                valid = False
+                if self.cleansiness<0:
+                    self.cleansiness=0
+                print(f"after washing {self.name} lost 3 cleansiness")
             elif clean == "2":
                 self.cleansiness +=2
-                valid == False
-            if self.cleansiness<0:
-                self.cleansiness=0
-            print(f"after brushing {self.name} lost 2 cleansiness")
-        else:
-            print("You must choose 1 or 2")
+                valid = False
+                if self.cleansiness<0:
+                    self.cleansiness=0
+                print(f"after brushing {self.name} lost 2 cleansiness")
+            else:
+                print("You must choose 1 or 2")
         print (f"Happiness: {self.happiness}  Tiredness: {self.tiredness} Hunger: {self.hunger} Cleansiness: {self.cleansiness} ")
 
     def rest(self):
@@ -125,26 +125,28 @@ class Pet:
         while valid == True:
             rest = input("How would you like to rest your pet")
             if rest == "1":
-             self.tiredness -=3
-             valid == False
-            if self.tiredness<0:self.tiredness=0
-            print(f"after sleeping {self.name} lost 3 tiredness")
-            if rest == "2":
+                self.tiredness -=3
+                valid = False
+                if self.tiredness<0:
+                    self.tiredness=0
+                print(f"after sleeping {self.name} lost 3 tiredness")
+            elif rest == "2":
                 self.tiredness -=2
-                valid == False
-            if self.tiredness<0:self.tiredness=0
-            print(f"after napping {self.name} lost 2 happiness")
-        else:
-            print("You must choose 1 or 2")
+                valid = False
+                if self.tiredness<0:
+                    self.tiredness=0
+                print(f"after napping {self.name} lost 2 happiness")
+            else:
+                print("You must choose 1 or 2")
         print (f"Happiness: {self.happiness}  Tiredness: {self.tiredness} Hunger: {self.hunger} Cleansiness: {self.cleansiness} ")
 
 
     def ignore(self):
-        self.happiness -=3
+        self.happiness -= 3
         self.tiredness += 3
         self.hunger += 3
         self.cleansiness -= 3
-        print(f"after ignoring {self.name} he lost 3 happiness, gained 3 tiredness, gained 3 hunger, gained 3 cleansiness")
+        print(f"after ignoring {self.name} he lost 3 happiness, gained 3 tiredness, gained 3 hunger, lost 3 cleansiness")
         print (f"Happiness: {self.happiness}  Tiredness: {self.tiredness} Hunger: {self.hunger} Cleansiness: {self.cleansiness} ")
             
 
@@ -172,7 +174,6 @@ def game(my_pet):
             Pet.ignore(my_pet)
         else:
             print("You have to choose 1,2,3,4, or 5.")
-            game(my_pet)
 game(my_pet)
 
 
